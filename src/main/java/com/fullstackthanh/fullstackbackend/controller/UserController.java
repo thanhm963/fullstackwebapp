890fb42a87266066ba401206caa09bab46bd649b
Thanh Ma <thanhm963@gmail.com>
2024-06-10 20:26:49 +0700

    custom exception handling

--- src/main/java/com/fullstackthanh/fullstackbackend/controller/UserController.java
+++ src/main/java/com/fullstackthanh/fullstackbackend/controller/UserController.java
@@ -1,17 +1,22 @@
 package com.fullstackthanh.fullstackbackend.controller;
 
+import java.nio.file.attribute.UserPrincipalNotFoundException;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fullstackthanh.fullstackbackend.exception.UserNotFoundException;
 import com.fullstackthanh.fullstackbackend.model.User;
 import com.fullstackthanh.fullstackbackend.repository.UserRepository;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
@@ -29,6 +34,12 @@ public class UserController {
     @GetMapping("/users")
     List<User> getAllUsers(){
         return userRepository.findAll();
+    }
+    
+    @GetMapping("/user/{id}")
+    User getUserById(@PathVariable Long id){
+        return userRepository.findById(id)
+                .orElseThrow(()->new UserNotFoundException(id));
     }
     
 }
