8cba16e11cd668f54f6e1026514ff4cac5becac0
Thanh Ma <thanhm963@gmail.com>
2024-06-18 20:29:36 +0700

    Edit user using PUTMapping

--- src/main/java/com/fullstackthanh/fullstackbackend/controller/UserController.java
+++ src/main/java/com/fullstackthanh/fullstackbackend/controller/UserController.java
@@ -18,6 +18,8 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PutMapping;
+
 
 
 @RestController
@@ -42,4 +44,15 @@ public class UserController {
                 .orElseThrow(()->new UserNotFoundException(id));
     }
     
+    @PutMapping("/user/{id}")
+    User updateUser(@RequestBody User newUser, @PathVariable Long id){
+        return userRepository.findById(id)
+                .map(user ->{
+                     user.setUsername(newUser.getUsername());
+                     user.setName(newUser.getName());
+                     user.setEmail(newUser.getEmail());
+                     return userRepository.save(user);
+                }).orElseThrow(() -> new UserNotFoundException(id));
+        
+    }
 }
